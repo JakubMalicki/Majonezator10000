6f7a2f82854c7854309aa459da6e3f19bd4439ce
JakubMalicki <126963638+JakubMalicki@users.noreply.github.com>
2023-04-16 21:37:51 +0200

    Basic Majonezator10000

---
+++ script.js
@@ -0,0 +1,69 @@
+var pages = {
+            'home': `<h1>Majonezator10000</h1>
+            <p>
+                Czy marzysz o oryginalnym majonezie o niepowtarzalnym smaku?<br>
+                Teraz możesz mieć go w domu dzięki urządzeniu, które wytwarza majonez z bluszczyku kurdybanka i zyzusiów tłuściochów - fantastycznych składników, których nie znajdziesz w żadnym sklepie spożywczym!<br><br>
+                Urządzenie do wytwarzania majonezu to rewolucyjne rozwiązanie dla miłośników kulinariów i smakoszy, którzy cenią sobie oryginalność i eksperymentowanie w kuchni. Dzięki niemu, możesz przygotować domowy majonez o wyjątkowym smaku i aromacie, który z pewnością zaskoczy Twoich gości.<br><br>
+                Składniki, takie jak bluszczyk kurdybanek i zyzusie tłuściochy, są naturalnymi i bezpiecznymi składnikami, które są wykorzystywane od wieków w kuchni. Mają one wyjątkowe właściwości smakowe i odżywcze, które dodadzą każdemu daniu niepowtarzalnego smaku i aromatu.<br><br>
+                Urządzenie do wytwarzania majonezu jest łatwe w obsłudze i umożliwia przygotowanie świeżego, naturalnego i pysznego majonezu w zaledwie kilka minut. To doskonały wybór dla osób, które cenią sobie zdrowe i smaczne jedzenie.<br><br>
+                Nie czekaj, zamów już dzisiaj urządzenie do wytwarzania majonezu i ciesz się oryginalnymi smakami w swojej kuchni!<br><br>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            </p>`,
+            'aboutus': `<h1>Mój brand®</h1>
+            <p>
+                Mój brand to firma, która od wielu lat specjalizuje się w produkcji wysokiej jakości i oryginalnych produktów spożywczych. Jesteśmy dumni z naszej pracy i pasji, którą wkładamy w każdy etap procesu produkcyjnego. Nasze wyroby to nie tylko smaczne dania, ale także prawdziwe dzieła sztuki kulinarnych, którymi można się delektować na co dzień.
+                <br><br>
+                Nasz najnowszy produkt to urządzenie do wytwarzania majonezu z fantastycznych składników, które zaskoczy Twoje kubki smakowe. Bluszczyk kurdybanka i zyzusie tłuściochy to niezwykłe składniki, które sprawią, że Twój majonez będzie wyjątkowy i niepowtarzalny. Nasze urządzenie to połączenie nowoczesnej technologii z tradycyjnymi składnikami, które są bezpieczne dla zdrowia i nie zawierają sztucznych dodatków.
+                <br><br>
+                Jako firma, która dba o jakość swoich produktów, mamy kontrolę nad każdym etapem procesu produkcyjnego. Składniki, które wykorzystujemy, pochodzą tylko z certyfikowanych źródeł, aby zapewnić naszym klientom produkt najwyższej jakości. Każde urządzenie, które wyjeżdża z naszej fabryki, jest starannie przetestowane i zatwierdzone przez naszych ekspertów.
+                <br><br>
+                Jesteśmy przekonani, że nasze urządzenie do wytwarzania majonezu z fantastycznych składników zadowoli nawet najbardziej wymagających smakoszy. Nasze innowacyjne rozwiązanie umożliwia szybkie i łatwe przygotowanie świeżego, naturalnego i pysznego majonezu w zaledwie kilka minut. To idealne rozwiązanie dla osób, które cenią sobie zdrowe i oryginalne jedzenie.
+                <br><br>
+                Dla naszej firmy najważniejsze jest zadowolenie naszych klientów. Dlatego oferujemy gwarancję satysfakcji i służymy pomocą na każdym etapie procesu zakupowego. Jesteśmy przekonani, że nasz produkt przyniesie wiele radości i zadowolenia każdemu, kto go wypróbuje.
+                <br><br>
+                Mój brand to nie tylko firma, ale także styl życia. Cenimy sobie zdrowie, smak i oryginalność. Nasze produkty są stworzone z myślą o ludziach, którzy cenią sobie najlepsze i najbardziej autentyczne smaki. Wybierz nasze urządzenie do wytwarzania majonezu z fantastycznych składników i dołącz do naszej grupy zadowolonych klientów!
+            </p>`,
+            'contact': `<h1>Kontakt</h1>
+            <p>
+                Jeśli masz jakieś pytania lub sugestie dotyczące naszych produktów, chętnie na nie odpowiemy. Skontaktuj się z nami za pomocą poniższych informacji kontaktowych:
+                <br><br>
+                Adres siedziby: ul. Kolorowa 10, 00-000 Miasto<br>
+                Telefon: +48 123 456 789<br>
+                Adres e-mail: kontakt@mojbrand.pl
+                <br><br>
+                Nasi pracownicy z przyjemnością pomogą Ci w każdej sprawie i udzielą niezbędnych informacji. Zawsze jesteśmy otwarci na feedback i sugestie, które pomogą nam w ciągłym doskonaleniu naszych produktów i usług.
+                <br><br>
+                Jeśli masz jakieś pytania dotyczące zamówienia lub dostawy, skontaktuj się z naszym działem obsługi klienta pod adresem e-mail: zamowienia@mojbrand.pl lub telefonicznie pod numerem +48 123 456 789. Nasi specjaliści z przyjemnością pomogą Ci w każdej kwestii.
+                <br><br>
+                Dziękujemy za zainteresowanie naszymi produktami i liczymy na dalszą współpracę!
+            </p>`
+        };
+
+        function getPageContent(page) {
+            var pageToReturn;
+            switch (page) {
+                case 'home':
+                    pageToReturn = pages.home;
+                    break;
+                case 'aboutus':
+                    pageToReturn = pages.aboutus;
+                    break;
+                case 'services':
+                    pageToReturn = pages.services;
+                    break;
+                case 'contact':
+                    pageToReturn = pages.contact;
+                    break;
+                default:
+                    pageToReturn = pages.home;
+                    break;
+            }
+            document.getElementById('content').innerHTML = pageToReturn;
+        }
